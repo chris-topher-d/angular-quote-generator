7b756b7dc64be3ce4f4aa2da5ff9a56c435dfc64
Christopher Dennis <cdennis.aus@gmail.com>
2019-03-21 22:22:44 -0500

    Removed Injectable - not needed

--- src/app/quote.service.ts
+++ src/app/quote.service.ts
@@ -5,11 +5,18 @@ export class QuoteService {
   private proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   private apiUri = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json=?';
 
-  constructor() {}
-
   getQuote() {
     const data = from(fetch(this.proxyUrl + this.apiUri)
       .then(res => res.text())
+      .then(quoteData => {
+        const parsedQuote = { quote: '', author: '' };
+        parsedQuote.quote = quoteData.slice(quoteData.indexOf('<quoteText>') + 11, quoteData.indexOf('</quoteText>'));
+        const author = quoteData.slice(quoteData.indexOf('<quoteAuthor>') + 13, quoteData.indexOf('</quoteAuthor>'));
+
+        // If author information isn't provided, assign "- uknown" as author
+        parsedQuote.author = author === '' ? '- uknown' : `- ${author}`;
+        return parsedQuote;
+      })
     );
 
     return data;
